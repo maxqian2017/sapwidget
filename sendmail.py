b4ff7a4fe7f92745fc7af864fd317543645931aa
maxqian2017 <31557233+maxqian2017@users.noreply.github.com>
2017-11-07 16:22:44 +0800

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,7 +1,7 @@
 import win32com.client as win32
 outlook = win32.Dispatch('outlook.application')
 mail = outlook.CreateItem(0)
-mail.To = 'max.qian@disney.com'
+mail.To = 'someone@somewhere.com'
 mail.Subject = 'Message subject'
 mail.Body = 'Message body'
 #mail.HTMLBody = '<h2>HTML Message body</h2>'# this field is optional
